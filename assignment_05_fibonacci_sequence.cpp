526fa2a3b679861cc2e1b0550a77a4ada9adf1d6
Suabira <your@email.com>
2026-08-01 15:57:35 +0000

    Implement assignments 1-5

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,62 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+void printFibonacci(int n) {
+    if (n <= 0) {
+        cout << "Error: Number of terms must be positive." << endl;
+        return;
+    }
+
+    int first = 0, second = 1;
+    cout << "Fibonacci sequence: ";
+    for (int i = 0; i < n; i++) {
+        cout << first << " ";
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+    cout << endl;
+}
+
+bool isFibonacci(int number) {
+    if (number < 0) {
+        return false;
+    }
+
+    int first = 0, second = 1;
+    if (number == 0) {
+        return true;
+    }
+
+    while (first <= number) {
+        if (first == number) {
+            return true;
+        }
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+    return false;
+}
+
+int main() {
+    int n;
+    cout << "How many terms? ";
+    cin >> n;
+    printFibonacci(n);
+
+    int number;
+    cout << "Enter a number to check: ";
+    cin >> number;
+
+    if (isFibonacci(number)) {
+        cout << number << " is a Fibonacci number." << endl;
+    } else {
+        cout << number << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
